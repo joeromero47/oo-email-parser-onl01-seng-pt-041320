8e933454a1db2b3fec06a728c609adaa032c20b0
Joseph Romero <joeromero47@gmail.com>
2020-05-09 21:42:48 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,16 +4,17 @@
 # or whitespace (' ').
 
 class EmailAddressParser
- attr_accessor :email 
- 
- def initialize(emails)
-   @email = emails
- end
- 
- def parse
-   email_array = email.split(/[, ]/).uniq
-   email_array.reject! {|element| element.empty?}
-   email_array
-   
- end
- end
+  attr_accessor :email 
+  
+  def initialize(emails)
+    @email = emails
+  end
+  
+  def parse
+    email_array = @email.split(/[, ]/).uniq 
+    email_array.reject! {|element| element.empty?}
+    email_array
+    
+  end
+end
+    
