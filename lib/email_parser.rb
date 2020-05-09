fbba5791d61764a07198f11bd0a28d23120c4d5b
Joseph Romero <joeromero47@gmail.com>
2020-05-09 21:26:31 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,7 +10,7 @@ class EmailParser
   end
   
   def parse
-    email_array = @mail.split(/[, ]/).uniq
+    email_array = @email.split(/[, ]/).uniq
     email_array.reject {|element| element.empty?}
     email_array
   end
