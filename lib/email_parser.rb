15893b5deaecc8e745ef7c87cc5dc3048a6c8d44
Joseph Romero <joeromero47@gmail.com>
2020-05-09 21:33:22 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,15 +4,16 @@
 # or whitespace (' ').
 
 class EmailParser
-  attr_accessor :emails
-  def initialize(emails)
-    @emails = emails
-  end
-  
-  def parse
-    puts splitEmail = emails.split.collect {|address| address.split(',')}
-    returnArr = splitEmail.flatten.uniq
-    returnArr
-  end
-  
+attr_accessor :emails 
+
+def initialize(emails)
+  @emails = emails
+end 
+
+def parse
+  puts splitEmail = emails.split.collect {|address| address.split(',')} 
+  returnArr = splitEmail.flatten.uniq
+  returnArr
+end
+
 end
